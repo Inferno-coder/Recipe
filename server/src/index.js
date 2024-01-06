986de4bb2da1b596d3d760e88af8527aeca54c68
Inferno-coder <pradeepceo18@gmail.com>
2024-01-06 23:57:10 +0530

    added

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-somthing
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+app.listen(3001,()=>console.log("Server Started"))
+console.log('pradeep is best coder');
